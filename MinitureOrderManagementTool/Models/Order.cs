bc66ce8cd7c23d96ef3f3f9818587bf34f597586
Takao Kaburaki <tacker.oct@live.jp>
2020-05-26 03:19:32 +0900

    Minor refactoring

--- MinitureOrderManagementTool/Models/Order.cs
+++ MinitureOrderManagementTool/Models/Order.cs
@@ -12,10 +12,10 @@ namespace MinitureOrderManagementTool.Models
         public DateTime CreatedAt { get; set; }
         public DateTime ModifiedAt { get; set; }
         public string Customer { get; set; }
-        public Decimal Price { get; set; }
+        public decimal Price { get; set; }
         public DateTime Deadline { get; set; }
         public Part[] Parts { get; set; }
         public string Description { get; set; }
-        public Decimal TimeSpent { get; set; }
+        public decimal TimeSpent { get; set; }
     }
 }
