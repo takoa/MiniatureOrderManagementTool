7d5c6c756a9fcef54b6c390438a34670f27ec0ff
Takao Kaburaki <tacker.oct@live.jp>
2020-05-30 04:58:50 +0900

    Update NumericTextBox
    Add dependency properties.
      - Maximum: limits the value
      - Minimum: limits the value
      - AllowRightClick: allows or prohibits right clicking
      - AllowPaste: allows or prohibits pasting
    Additionally, the implementation was dirty and hacky, so re-implemented to be more straightforward.

--- MiniatureOrderManagementTool/Controls/NumericTextBox.cs
+++ MiniatureOrderManagementTool/Controls/NumericTextBox.cs
@@ -6,43 +6,141 @@ namespace MiniatureOrderManagementTool.Controls
 {
     public class NumericTextBox : TextBox
     {
+        private static readonly KeyBinding pasteKeyBinding1 = new KeyBinding(ApplicationCommands.NotACommand, Key.V, ModifierKeys.Control);
+        private static readonly KeyBinding pasteKeyBinding2 = new KeyBinding(ApplicationCommands.NotACommand, Key.Insert, ModifierKeys.Shift);
+
+        private string oldText = string.Empty;
+
+        public bool AllowRightClick
+        {
+            get => (bool)this.GetValue(AllowRightClickProperty);
+            set => this.SetValue(AllowRightClickProperty, value);
+        }
+
+        public static readonly DependencyProperty AllowRightClickProperty =
+            DependencyProperty.Register("AllowRightClick", typeof(bool), typeof(NumericTextBox), new PropertyMetadata(true, OnAllowRightClickChanged));
+
+        public bool AllowPaste
+        {
+            get { return (bool)this.GetValue(AllowPasteProperty); }
+            set { this.SetValue(AllowPasteProperty, value); }
+        }
+
+        public static readonly DependencyProperty AllowPasteProperty =
+            DependencyProperty.Register("AllowPaste", typeof(bool), typeof(NumericTextBox), new PropertyMetadata(true, OnAllowPasteChanged));
+
+        public decimal Maximum
+        {
+            get { return (decimal)this.GetValue(MaximumProperty); }
+            set { this.SetValue(MaximumProperty, value); }
+        }
+
+        public static readonly DependencyProperty MaximumProperty =
+            DependencyProperty.Register("Maximum", typeof(decimal), typeof(NumericTextBox), new PropertyMetadata(decimal.MaxValue, OnMaximumChanged));
+
+        public decimal Minimum
+        {
+            get { return (decimal)this.GetValue(MinimumProperty); }
+            set { this.SetValue(MinimumProperty, value); }
+        }
+
+        public static readonly DependencyProperty MinimumProperty =
+            DependencyProperty.Register("Minimum", typeof(decimal), typeof(NumericTextBox), new PropertyMetadata(decimal.MinValue, OnMinimumChanged));
+
         static NumericTextBox()
         {
-            // IMEを無効化。
             InputMethod.IsInputMethodEnabledProperty.OverrideMetadata(typeof(NumericTextBox), new FrameworkPropertyMetadata(false));
         }
 
         public NumericTextBox()
         {
-            // ペーストのキーボードショートカットを無効化。
-            this.InputBindings.Add(new KeyBinding(ApplicationCommands.NotACommand, Key.V, ModifierKeys.Control));
-            this.InputBindings.Add(new KeyBinding(ApplicationCommands.NotACommand, Key.Insert, ModifierKeys.Shift));
         }
 
-        protected override void OnPreviewKeyDown(KeyEventArgs e)
+        protected override void OnPreviewMouseRightButtonUp(MouseButtonEventArgs e)
         {
-            // 数値以外、または数値の入力に関係しないキーが押された場合、イベントを処理済みに。
-            if (!((Key.D0 <= e.Key && e.Key <= Key.D9) ||
-                  (Key.NumPad0 <= e.Key && e.Key <= Key.NumPad9) ||
-                  Key.Back == e.Key ||
-                  Key.Delete == e.Key ||
-                  Key.Tab == e.Key ||
-                  Key.Left == e.Key ||
-                  Key.Right == e.Key ||
-                  Key.Up == e.Key ||
-                  Key.Down == e.Key) ||
-                (Keyboard.Modifiers & ModifierKeys.Shift) > 0)
+            if (!this.AllowRightClick)
             {
                 e.Handled = true;
             }
-
-            base.OnKeyDown(e);
         }
 
-        // 右クリックを無効化。
-        protected override void OnPreviewMouseRightButtonUp(MouseButtonEventArgs e)
+        protected override void OnTextChanged(TextChangedEventArgs e)
         {
-            e.Handled = true;
+            if (decimal.TryParse(this.Text, out var d))
+            {
+                if (this.Minimum <= d && d <= this.Maximum)
+                {
+                    this.oldText = this.Text;
+                }
+                else
+                {
+                    this.Text = this.oldText;
+                }
+            }
+            else
+            {
+                this.Text = this.oldText;
+            }
+
+            base.OnTextChanged(e);
+        }
+
+        protected virtual void OnAllowRightClickChanged(DependencyPropertyChangedEventArgs e)
+        {
+        }
+
+        protected virtual void OnAllowPasteChanged(DependencyPropertyChangedEventArgs e)
+        {
+            if ((bool)e.NewValue)
+            {
+                this.InputBindings.Remove(pasteKeyBinding1);
+                this.InputBindings.Remove(pasteKeyBinding2);
+            }
+            else
+            {
+                this.InputBindings.Add(pasteKeyBinding1);
+                this.InputBindings.Add(pasteKeyBinding2);
+            }
+        }
+
+        protected virtual void OnMaximumChanged(DependencyPropertyChangedEventArgs e)
+        {
+        }
+
+        protected virtual void OnMinimumChanged(DependencyPropertyChangedEventArgs e)
+        {
+        }
+
+        private static void OnAllowRightClickChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is NumericTextBox ntb)
+            {
+                ntb.OnAllowRightClickChanged(e);
+            }
+        }
+
+        private static void OnAllowPasteChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is NumericTextBox ntb)
+            {
+                ntb.OnAllowPasteChanged(e);
+            }
+        }
+
+        private static void OnMaximumChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is NumericTextBox ntb)
+            {
+                ntb.OnMaximumChanged(e);
+            }
+        }
+
+        private static void OnMinimumChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is NumericTextBox ntb)
+            {
+                ntb.OnMinimumChanged(e);
+            }
         }
     }
 }
