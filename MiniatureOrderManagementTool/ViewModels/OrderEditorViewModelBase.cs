c4314ec030dd90e310ca1e8718c4b47b0c08e13d
Takao Kaburaki <tacker.oct@live.jp>
2020-07-10 05:00:45 +0900

    Refactoring (cleanup)

--- MiniatureOrderManagementTool/ViewModels/OrderEditorViewModelBase.cs
+++ MiniatureOrderManagementTool/ViewModels/OrderEditorViewModelBase.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace MiniatureOrderManagementTool.ViewModels
+﻿namespace MiniatureOrderManagementTool.ViewModels
 {
     public class OrderEditorViewModelBase : CommonOrderEditorViewModel
     {
