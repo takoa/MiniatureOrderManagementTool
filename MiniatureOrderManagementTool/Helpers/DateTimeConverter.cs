ea22e331fb442311c928dd426e475ad2298af433
Takao Kaburaki <tacker.oct@live.jp>
2020-07-18 02:24:03 +0900

    Update DateTime display in the stock tab
    Change the display from the default to "yyyy/MM/dd HH:mm"

---
+++ MiniatureOrderManagementTool/Helpers/DateTimeConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MiniatureOrderManagementTool.Helpers
+{
+    public class DateTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime dt)
+            {
+                return dt.ToString("yyyy/MM/dd HH:mm");
+            }
+            else
+            {
+                return value.ToString();
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
