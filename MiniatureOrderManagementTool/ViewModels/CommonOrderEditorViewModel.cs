c7d7d47e9b4033dc4a5cb33320f956e36540c786
Takao Kaburaki <tacker.oct@live.jp>
2020-08-08 17:14:47 +0900

    Refactoring (cleanup)

--- MiniatureOrderManagementTool/ViewModels/CommonOrderEditorViewModel.cs
+++ MiniatureOrderManagementTool/ViewModels/CommonOrderEditorViewModel.cs
@@ -3,7 +3,6 @@ using MiniatureOrderManagementTool.Models;
 using MiniatureOrderManagementTool.Models.Dtos;
 using ReactiveUI;
 using System;
-using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.Reactive;
 
