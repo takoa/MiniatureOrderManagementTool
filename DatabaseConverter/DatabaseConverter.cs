65463e466aafb7f0ba5cd84b8b550cd354f234fa
Takao Kaburaki <tacker.oct@live.jp>
2020-08-12 10:16:54 +0900

    Add DatabaseConverter project

---
+++ DatabaseConverter/DatabaseConverter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MiniatureOrderManagementTool.DatabaseConverter
+{
+    public class DatabaseConverter
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
